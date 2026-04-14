ec52e1a8b80aa71356a4ac612e67b7f9f70effd0
Hanna Lindkvist <hali2507@student.miun.se>
2026-04-14 13:46:10 +0200

    konfigurerar server för express, databas, cors, json, port

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+//Paket
 const express = require("express");
+const Database = require("better-sqlite3");
+const cors = require("cors");
+
+//Databasanslutning
+const db = new Database("jobs.db");
+
+//Varibel för express
 const app = express();
+
+//Cross-origin
+app.use(cors());
+
+//Parse JSON
+app.use(express.json());
+
+//Starta server
 const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});

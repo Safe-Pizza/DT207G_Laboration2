ccdce5157c3f2a1468f8ffc58828df5fbc1077d8
Hanna Lindkvist <hali2507@student.miun.se>
2026-04-14 13:36:26 +0200

    skapar install-fil för databas + tabell

---
+++ install.js
@@ -0,0 +1,18 @@
+//Databasanslutning
+const Database = require("better-sqlite3");
+const db = new Database("jobs.db");
+
+db.exec(`
+        CREATE TABLE job (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        companyname TEXT NOT NULL,
+        jobtitle TEXT NOT NULL,
+        location TEXT NOT NULL,
+        descripton TEXT NOT NULL,
+        startdate TEXT NOT NULL,
+        enddate TEXT NOT NULL,
+        user_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+        )`);
+
+
+db.close();

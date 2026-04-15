9f5fbd6697adb39dccae78ec3990d4fbee34a0ab
Hanna Lindkvist <hali2507@student.miun.se>
2026-04-15 15:47:35 +0200

    uppdaterar kommentarer

--- routes/job.js
+++ routes/job.js
@@ -25,6 +25,7 @@ route.get("/:id", (req, res) => {
     let jobId = req.params.id;
 
     try {
+        //Kontroll ifall ID är undefined
         if (jobId === undefined) {
             res.status(400).json({ message: "ID is not recognized" });
 
@@ -33,6 +34,7 @@ route.get("/:id", (req, res) => {
         //Hämta specifik data från databas
         const job = db.prepare("SELECT * FROM job WHERE id = ?").get(jobId);
 
+        //returnera svar från sql-fråga
         res.json(job);
     } catch (error) {
         res.status(500).json({ message: `An error has occurd: ${error}` })
@@ -47,6 +49,7 @@ route.post("/", (req, res) => {
     let startdate = req.body.startdate;
     let enddate = req.body.enddate;
 
+    //varibel för felmeddelande
     let errors = {
         message: "",
         detail: "",
@@ -55,10 +58,12 @@ route.post("/", (req, res) => {
         }
     };
 
+    //Kontroll inga tomma textfält
     if (!companyname || !jobtitle || !location || !description || !startdate || !enddate) {
+        //Felmeddelande
         errors.message = "All params are not included";
         errors.detail = "Must include companyname, jobtitle, location, description, startdate, enddate in JSON"
-
+        //Felkods-status
         errors.https_res.message = "Bad request";
         errors.https_res.code = 400;
 
@@ -77,10 +82,13 @@ route.post("/", (req, res) => {
     }
 
     try {
+        //SQL-fråga lägga till i databas
         const jobInput = db.prepare(`INSERT INTO job(companyname, jobtitle, location, descripton, startdate, enddate)VALUES(?, ?, ?, ?, ?, ?);`);
         jobInput.run(companyname, jobtitle, location, description, startdate, enddate);
-        res.json({ message: "Job has been added " + job.jobtitle });
+        //Meddelande vid OK
+        res.status(201).json({ message: "Job has been added " + job.jobtitle });
     } catch (error) {
+        //Felmeddelande
         res.status(500).json({ message: "Error occured: " + error });
     }
 })
@@ -93,18 +101,22 @@ route.put("/:id", (req, res) => {
 route.delete("/:id", (req, res) => {
     let jobId = req.params.id;
 
+    //Kontroll om ID undefined
     if (jobId === undefined) {
+        //Felmeddelande
         res.status(400).json({ message: "ID is not recognized" });
 
         return;
     }
     try {
+        //SQL-fråga ta bort från databas
         const deleteInput = db.prepare(`DELETE FROM job WHERE id = ?;`);
-
+        //Kör SQL-frpga
         deleteInput.run(jobId);
-
+        //Meddelande vid OK
         res.json({ message: `DELETE request OK, ID: ${jobId} deleted` });
     } catch (error) {
+        //Felmeddelande
         res.status(500).json({ message: `An error has occured: ${error}` });
     }
 })

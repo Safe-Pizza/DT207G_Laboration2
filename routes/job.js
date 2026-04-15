bdce948bdb77272f2de34a12cd464b957180a849
Hanna Lindkvist <hali2507@student.miun.se>
2026-04-15 14:30:39 +0200

    lägger till routes + skriver post request felmeddelande

--- routes/job.js
+++ routes/job.js
@@ -8,14 +8,67 @@ const route = express.Router();
 route.get("/", (req, res) => {
     try {
         const jobs = db.prepare("SELECT * FROM job").all();
-        res.json(jobs);
+
+        if (jobs.length === 0) {
+            res.status(404).json({ message: "No jobs found" });
+        } else {
+            res.json(jobs);
+        }
     } catch (error) {
         res.status(500).json({ message: "Could not fetch jobs" });
     }
 })
 
 route.get("/:id", (req, res) => {
-    res.json({ message: 'GET request: job/:id' });
+    res.json({ message: "GET request: job/:id" });
 })
 
+route.post("/", (req, res) => {
+    let companyname = req.body.companyname;
+    let jobtitle = req.body.jobtitle;
+    let location = req.body.location;
+    let description = req.body.descripton;
+    let startdate = req.body.startdate;
+    let enddate = req.body.enddate;
+
+    let errors = {
+        message: "",
+        detail: "",
+        https_res: {
+
+        }
+    };
+
+    if(!companyname || !jobtitle || !location || !description || !startdate || !enddate) {
+        errors.message = "All params are not included";
+        errors.detail = "Must include companyname, jobtitle, location, description, startdate, enddate in JSON"
+
+        errors.https_res.message = "Bad request";
+        errors.https_res.code = 400;
+
+        res.status(400).json(errors);
+
+        return;
+    }
+
+    let job = {
+        companyname: companyname,
+        jobtitle: jobtitle,
+        location: location,
+        description: description,
+        startdate: startdate,
+        enddate: enddate
+    }
+
+    res.json({ message: "POST request job" });
+})
+
+
+route.put("/:id", (req, res) => {
+    res.json({ message: "PUT request job/:id" });
+})
+
+route.delete("/:id", (req, res) => {
+    res.json({ message: "DELETE request job:id" })
+})
 module.exports = route;
